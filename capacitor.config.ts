e41e62e5d63e8286d5d22681f0a26b641ddbd424
George “Casper” Virden IV <virden26@gmail.com>
2025-12-17 07:24:59 +0000

    fix internal server error

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.agendaplus',
+  appName: 'Agenda+',
+  webDir: 'out'
+};
+
+export default config;
