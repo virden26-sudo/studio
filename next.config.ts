bc87900a8c3eb16026d4925131b0ec64807ae595
George “Casper” Virden IV <virden26@gmail.com>
2025-12-17 07:05:52 +0000

    get webapp ready

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
