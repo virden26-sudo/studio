7dd8b6f8046bbeeb8c9116bdd75ae26fe894368f
George “Casper” Virden IV <virden26@gmail.com>
2025-12-19 05:28:52 +0000

    sorry folder

---
+++ FinalA+/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"povuwGYL5RzGhMTnjNrfL6APgakaGGEaf36KSFTi6S4=\"\n}"
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  distDir: 'FinalA+',
   output: 'standalone',
   serverActions: {
     bodySizeLimit: '10mb',
